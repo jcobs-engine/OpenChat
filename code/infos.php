08bcfdc84c018709f95a9f0e68cf82c2ba1dfeab
Jcobs-Engine <levi@levi-jacobs.de>
2019-10-06 15:22:55 +0200

    v1.0.0

---
+++ code/infos.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>
+            OpenChat | Messenger
+        </title>    
+    </head>
+
+    <body style="background-color:#070707;">
+        <!-- Release-List -->
+        <div style="width:100%; height:100%; position:absolute; top:0px; left:0px;">    
+            <div style='width:100%; padding-top:5px;height:60px; margin-bottom:20px;border-radius:0px; box-shadow:0px 0px 3px 5px #d1d1d1; background-color:#d1d1d1; text-align:center; color:black;font-size:40px;text-shadow:0px 0px 2px black;display:block;'><b><span style='color:rgb(20%, 20%, 20%);'>v</span>0.0.0</b></div>
+            
+            <div style='width:100%; height:45px; margin-bottom:10px;border-radius:0px; box-shadow:0px 0px 3px 5px #d1d1d1; background-color:#d1d1d1; text-align:center; color:black;font-size:30px;text-shadow:0px 0px 2px black;display:block;'>$message</div>
+        
+
+        </div>
+    </body>
+</html>
